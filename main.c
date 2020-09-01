42e4ff547bdeefcbccf37032ce047dbfd9fb47c1
octaviasz <oas5135@psu.edu>
2020-09-01 13:23:42 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+// Author: Octavia Szkutnik oas5135@psu.edu
+// Collaborator: Daniel Mikita djm6907@psu.edu
+// Collaborator: Joseph Imperial jri5073@psu.edu
+// Collaborator: Mya Romano mlr5819@psu.edu
+#include <stdio.h>
+int main()
+{
+    float celsius, fahrenheit;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("%.6f Celsius = %.6f Fahrenheit", celsius, fahrenheit);
+
+    return 0;
+}
